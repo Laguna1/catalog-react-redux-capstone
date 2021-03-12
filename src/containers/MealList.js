7034e7765babf6599a3829483bcc1e34c88452ff
Laguna1 <lagunazoomarket@gmail.com>
2021-03-12 19:06:41 +0200

    Add mapStateToProps and connect MealList to the store

--- src/containers/MealList.js
+++ src/containers/MealList.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function MealList() {
+const MealList = ({ meals }) => {
+//   return (
+//     <div className="meal-list">
+//       <h1>MealList here!</h1>
+//     </div>
+//   );
+  const addMeals = meals.map(meal => (
+    <div className="meal-add" key={meals.id}>
+      <h2>{meals.title}</h2>
+      <div>{meals.content.substring(0, 100)}</div>
+    </div>
+  ));
+
   return (
-    <div className="meal-list">
-      <h1>MealList here!</h1>
+    <div>
+      <h1>All Meals</h1>
+      <div className="meals-list">
+        {addMeals}
+      </div>
     </div>
   );
-}
+};
 
-export default MealList;
+const mapStateToProps = state => ({
+  meals: state.meals,
+});
+
+export default connect(mapStateToProps, null)(MealList);
